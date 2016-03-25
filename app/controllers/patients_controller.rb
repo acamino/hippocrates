131849ed00bd409e44c9419a4e2f39d276d16117
Agustin Camino <agustin.camino@gmail.com>
2016-03-25 11:52:27 -0500

    Retrieve patients from the database

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,5 @@
 class PatientsController < ApplicationController
   def index
+    @patients = Patient.all
   end
 end
--- spec/controllers/patients_controller_spec.rb
+++ spec/controllers/patients_controller_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 
 describe PatientsController do
   describe '#index' do
-    before { get :index }
+    let(:patients) { [double(:patient)] }
 
-    it 'responds with ok' do
-      expect(response).to be_ok
+    before do
+      allow(Patient).to receive(:all) { patients }
+      get :index
+    end
+
+    it 'assings @patients' do
+      expect(assigns(:patients)).to eq(patients)
     end
 
     it 'renders the index template' do
       expect(response).to render_template(:index)
+    end
+
+    it 'responds with ok' do
+      expect(response).to be_ok
     end
   end
 end
