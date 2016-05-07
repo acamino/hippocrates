492a010c0c29ee9de25153f46da547dbb55d6db4
Agustin Camino <agustin.camino@gmail.com>
2016-05-07 07:30:05 -0500

    Add factory for consultations

---
+++ spec/factories/consultations.rb
@@ -0,0 +1,28 @@
+FactoryGirl.define do
+  factory :consultation do
+    reason               'reason'
+    ongoing_issue        'ongoing issue'
+    organs_examination   'organs examination'
+    temperature          'temperature'
+    heart_rate           'heart rate'
+    blood_pressure       'blood pressure'
+    respiratory_rate     'respiratory rate'
+    weight               'weight'
+    height               'height'
+    physical_examination 'physical examination'
+    right_ear            'right ear'
+    left_ear             'left ear'
+    right_nostril        'right nostril'
+    left_nostril         'left nostril'
+    nasopharynx          'nasopharynx'
+    nose_others          'nose others'
+    oral_cavity          'oral cavity'
+    oropharynx           'oropharynx'
+    hypopharynx          'hypopharynx'
+    larynx               'larynx'
+    neck                 'neck'
+    others               'others'
+
+    patient
+  end
+end
