01000e2b6dd08eef112688ee952bf9a1a8ae1271
Agustin Camino <agustin.camino@gmail.com>
2016-06-06 06:04:38 -0500

    Add predicate methods to anamnesis

--- app/models/anamnesis.rb
+++ app/models/anamnesis.rb
@@ -1,3 +1,15 @@
 class Anamnesis < ActiveRecord::Base
   belongs_to :patient
+
+  def allergies?
+    allergies.present?
+  end
+
+  def observations?
+    observations.present?
+  end
+
+  def personal_history?
+    personal_history.present?
+  end
 end
--- spec/models/anamnesis_spec.rb
+++ spec/models/anamnesis_spec.rb
@@ -4,4 +4,30 @@ describe Anamnesis do
   describe 'associations' do
     it { is_expected.to belong_to(:patient) }
   end
+
+  describe '#allergies?' do
+    it 'returns true when allergies are not empty' do
+      anamnesis = build(:anamnesis, allergies: 'PNC')
+      expect(anamnesis.allergies?).to be_truthy
+    end
+
+    it 'returns false when allergies are empty' do
+      anamnesis = build(:anamnesis, allergies: '')
+      expect(anamnesis.allergies?).to be_falsey
+    end
+  end
+
+  describe '#observations?' do
+    it 'returns true when observations are not empty' do
+      anamnesis = build(:anamnesis, observations: 'pnc')
+      expect(anamnesis.observations?).to be_truthy
+    end
+  end
+
+  describe '#personal_history?' do
+    it 'returns true when personal_history is not empty' do
+      anamnesis = build(:anamnesis, personal_history: 'pnc')
+      expect(anamnesis.personal_history?).to be_truthy
+    end
+  end
 end
