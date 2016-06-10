4af111189207aec91f540f5ce89583f7290bd58f
Agustin Camino <agustin.camino@gmail.com>
2016-06-10 06:09:52 -0500

    Add `.most_recent` to consultation
    Also add `miscellaneous?` and `next_appointment?`.

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -10,4 +10,18 @@ class Consultation < ActiveRecord::Base
   accepts_nested_attributes_for :prescriptions,
                                 reject_if: -> (attributes) { attributes[:inscription].blank? },
                                 allow_destroy: true
+
+  scope :ordered_by_date, -> { order(created_at: :asc) }
+
+  def self.most_recent
+    ordered_by_date.last
+  end
+
+  def miscellaneous?
+    miscellaneous.present?
+  end
+
+  def next_appointment?
+    next_appointment.present?
+  end
 end
--- spec/models/consultation_spec.rb
+++ spec/models/consultation_spec.rb
@@ -11,4 +11,30 @@ describe Consultation do
     it { is_expected.to accept_nested_attributes_for(:diagnoses).allow_destroy(true) }
     it { is_expected.to accept_nested_attributes_for(:prescriptions).allow_destroy(true) }
   end
+
+  describe '.most_recent' do
+    let!(:old_consultation)    { create :consultation, created_at: 1.hour.ago }
+    let!(:recent_consultation) { create :consultation, created_at: Time.now }
+
+    it 'returns the most recent consultation' do
+      expect(Consultation.most_recent).to eq(recent_consultation)
+    end
+  end
+
+  describe '#miscellaneous?' do
+    let(:consultation) { build(:consultation) }
+
+    context 'when miscellaneous is present' do
+      it 'returns true' do
+        consultation.miscellaneous = 'notes'
+        expect(consultation.miscellaneous?).to be_truthy
+      end
+    end
+
+    context 'when miscellaneous is not present' do
+      it 'returns false' do
+        expect(consultation.miscellaneous?).to be_falsey
+      end
+    end
+  end
 end
