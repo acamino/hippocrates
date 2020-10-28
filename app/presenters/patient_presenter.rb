775d02ef301b626a7a579f48e01c17ac4f3f0d4c
Ma. Elena Bustillos <ma.elena.bustillos@gmail.com>
2020-10-28 12:07:33 +0000

    Add birthdate to the certificate

--- app/presenters/patient_presenter.rb
+++ app/presenters/patient_presenter.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 class PatientPresenter < SimpleDelegator
+  attr_writer :consultation_date
+
   def age
-    AgeCalculator.calculate(birthdate)
+    @age ||= AgeCalculator.calculate(birthdate)
+  end
+
+  def formatted_age
+    relative_age = AgeCalculator.calculate(birthdate, @consultation_date)
+    years  = relative_age.years
+    months = relative_age.months
+    years_label  = years  == 1 ? 'AÑO' : 'AÑOS'
+    months_label = months == 1 ? 'MES' : 'MESES'
+    return "#{months} #{months_label}" if years.zero?
+    return "#{years} #{years_label}"   if months.zero?
+    "#{years} #{years_label} #{months} #{months_label}"
   end
 
   def formatted_birthdate
--- spec/services/age_calculator_spec.rb
+++ spec/services/age_calculator_spec.rb
@@ -2,9 +2,19 @@ require 'timecop'
 require_relative '../../app/services/age_calculator'
 
 describe AgeCalculator do
-  it 'calculates age in years and months' do
-    Timecop.freeze('2011-07-29') do
-      age = described_class.calculate(Date.new(1979, 9, 10))
+  context 'when date is *NOT* passed' do
+    it 'calculates age in years and months' do
+      Timecop.freeze('2011-07-29') do
+        age = described_class.calculate(Date.new(1979, 9, 10))
+        expect(age.years).to eq(31)
+        expect(age.months).to eq(11)
+      end
+    end
+  end
+
+  context 'when date is passed' do
+    it 'calculates age in years and months' do
+      age = described_class.calculate(Date.new(1979, 9, 10), Date.new(2011, 7, 29))
       expect(age.years).to eq(31)
       expect(age.months).to eq(11)
     end
