147bb3c88c8fe0ea0a1566dc6fb3779fbb13148c
Ma. Elena Bustillos <ma.elena.bustillos@gmail.com>
2021-09-18 22:08:33 -0500

    Prevent unnecessary XHR requests (#81)
    - The application must call the endpoints to fetch diseases and medicines only
      when creating or updating a consultation.

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,24 +1,28 @@
 Hippocrates.Autocomplete = {
   init: function() {
-    $.get("/api/diseases").done(function(suggestions) {
-      $(".disease").autocomplete({
-        lookup: suggestions,
-        onSelect: function (suggestion) {
-          $(this).closest("tr").find("input.code").val(suggestion.data);
-        }
-      });
-    });
-
-    $.get("/api/medicines").done(function(medicines) {
-      $(".inscription").autocomplete({
-        lookup: medicines,
-        onSelect: function (medicine) {
-          var locked = $(this).closest("tr").find(".locked").is(':checked');
-          if (!locked) {
-            $(this).closest("tr").find("input.subscription").val(medicine.data);
+    if ($('.diagnoses').length > 0) {
+      $.get("/api/diseases").done(function(suggestions) {
+        $(".disease").autocomplete({
+          lookup: suggestions,
+          onSelect: function (suggestion) {
+            $(this).closest("tr").find("input.code").val(suggestion.data);
           }
-        }
+        });
       });
-    });
+    }
+
+    if ($('.prescriptions').length > 0) {
+      $.get("/api/medicines").done(function(medicines) {
+        $(".inscription").autocomplete({
+          lookup: medicines,
+          onSelect: function (medicine) {
+            var locked = $(this).closest("tr").find(".locked").is(':checked');
+            if (!locked) {
+              $(this).closest("tr").find("input.subscription").val(medicine.data);
+            }
+          }
+        });
+      });
+    }
   }
 };
