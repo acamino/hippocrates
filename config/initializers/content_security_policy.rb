88146633ae200a97c55d2eda671461e5bd8bc7d5
Agustin Camino <agustin.camino@gmail.com>
2026-02-12 15:14:28 -0500

    security: Enable Content-Security-Policy in report-only mode
    Adds CSP headers allowing self, cdnjs.cloudflare.com, and
    maxcdn.bootstrapcdn.com for scripts/styles/fonts. Uses report-only
    mode to detect violations without blocking resources.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,27 +4,16 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
-#   # If you are using webpack-dev-server then specify webpack-dev-server host
-#   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self
+  policy.font_src    :self, 'cdnjs.cloudflare.com'
+  policy.img_src     :self, :data
+  policy.object_src  :none
+  policy.script_src  :self, :unsafe_inline, 'cdnjs.cloudflare.com', 'maxcdn.bootstrapcdn.com'
+  policy.style_src   :self, :unsafe_inline, 'cdnjs.cloudflare.com', 'maxcdn.bootstrapcdn.com'
+  policy.connect_src :self
+end
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
-
-# If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
-
-# Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
+# Start in report-only mode to detect violations without blocking resources.
+# Monitor logs/reports, then switch to enforcing once no violations are seen.
+Rails.application.config.content_security_policy_report_only = true
